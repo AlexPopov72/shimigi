400ca66d262ee107f5640c65070c4d168b9e4ba2
Alex <al.vl.popov@mail.ru>
2022-04-26 13:44:20 +0500

    menu]

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://raw.githubusercontent.com/AlexPopov72/shimigi/main/src/accepts/menu.json";
+const baseUrl = "https://raw.githubusercontent.com/AlexPopov72/shimigi/02Cart/src/js/menu.json";
 let categoryList = fetch(baseUrl)
     .then(response => response.json())
     .then(data => {
@@ -13,8 +13,8 @@ function setNavLinks(data) {
         Object.entries(data).forEach(item => {
             if (item[1].status == true) {
                 let navElenet = `<li class="menutop__li"><a href="#">${item[1].name}</a></li>`
-                }
                 navBlock.innerHTML += navElenet;
+            }
         })
     }
 }
